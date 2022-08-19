c35a21e1c287e06b3b2ea11ebc091b8206ef6d52
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-08-19 22:46:29 +0700

    Third test

--- Program.cs
+++ Program.cs
@@ -35,3 +35,16 @@ else
     }
 }
 */
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write ("Input integer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine ($"{num} is even");
+}else 
+{
+    Console.WriteLine ($"{num} is odd");
+}
+*/
