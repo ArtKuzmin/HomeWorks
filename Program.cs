f45106e148d82d4f98e4e8defb50269d051777d8
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-08-19 22:41:59 +0700

    Secont test

--- Program.cs
+++ Program.cs
@@ -10,3 +10,28 @@ if (num1 > num2)
     Console.WriteLine ($"{num2} is bigger than {num1}");
 }
 */
+/*
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Input three integers: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine($"The max of three integers is: {num1}");
+    }
+}
+else
+{
+    if (num3 > num2)
+    {
+        Console.WriteLine($"The max of three integers is: {num3}");
+    }
+    else
+    {
+        Console.WriteLine($"The max of three integers is: {num2}");
+    }
+}
+*/
