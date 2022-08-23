5b692b0d8df36bd9d11f5bc9ab8135cd60570c74
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-08-23 22:25:56 +0700

    1 test completed

--- C#Homework_2/Program.cs
+++ C#Homework_2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/*
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int ShowSecondDigit (int numder)
+{
+    
+    int result = (numder % 100)/10;
+    return result;
+}
+
+Console.Write ("Input 3-digit integer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int secondDigit = ShowSecondDigit(num);
+Console.Write ($" Second digit of number {num} is {secondDigit}");
+*/
+
