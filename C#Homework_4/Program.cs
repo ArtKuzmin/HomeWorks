424ea3d51152ff654924b4017d76cce41035e2f2
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-08-30 19:11:26 +0700

    2 twst completed

--- C#Homework_4/Program.cs
+++ C#Homework_4/Program.cs
@@ -14,3 +14,32 @@ int b = Convert.ToInt32(Console.ReadLine());
 int pow = ToPower(a, b);
 Console.Write($"{a} в степени {b} равняется {pow}");
 */
+
+/*
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int FindIndex(int number)
+{
+    int result = number;
+    int index = 0;
+    for (int i = 0; result >= 1; i++)
+    {
+        result = result / 10;
+        index++;
+    }
+    return index;
+}
+int isNumberPalindrom(int index, int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= index; i++)
+    {
+        sum += (int)((number % Math.Pow(10, i)) / Math.Pow(10, i - 1));
+    }
+    return sum;
+}
+Console.Write("Input integer: ");
+int numIn = Convert.ToInt32(Console.ReadLine());
+int index = FindIndex(numIn);
+int sum = isNumberPalindrom(index, numIn);
+Console.Write(sum);
+*/
