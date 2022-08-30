cc6a889e8df91b78da2ee683ec5dac6cedaf0f85
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-08-30 15:41:30 +0700

    1 test completed

--- C#Homework_4/Program.cs
+++ C#Homework_4/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/*
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int ToPower(int a, int b)
+{
+    int pow = a;
+    for (int i = 1; i < b; i++)
+        pow *= a;
+    return pow;
+}
+Console.Write("Input integer A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input integer B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int pow = ToPower(a, b);
+Console.Write($"{a} в степени {b} равняется {pow}");
+*/
