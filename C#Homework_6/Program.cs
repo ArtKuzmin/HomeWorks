a5331c1be8229c6980222f9b83870930bbe4a3e5
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-09-07 17:56:45 +0700

    1 test completed

--- C#Homework_6/Program.cs
+++ C#Homework_6/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Как вариант, можно без использования массива в цикле сразу после запроса числа проверять 
+//его на положительность и увеличивать или нет счетчик. Альтернативное решение закомментировано в коде
+
+
+//int FindNumberPositive(int m) 
+int FindNumberPositive(int[] array)
+{
+    int count = 0;
+    //for (int i = 0; i < m; i++)
+    for (int i = 0; i < array.Length; i++)
+    {
+        // Console.Write($"Input {i} number: ");
+        // int a = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+        //if (a > 0)
+            count++;
+    }
+    return count;
+}
+
+int[] CreateArray(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Input {i} number: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+Console.Write("How many numbers do you want to input? ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(m);
+//int count = FindNumberPositive(m);
+int count = FindNumberPositive(arr);
+Console.WriteLine($"QUantity of positive numbers is {count}");
