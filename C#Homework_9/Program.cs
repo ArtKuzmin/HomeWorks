ab593a658411d77dd9c336816b6e3060e590f89b
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-09-17 23:24:39 +0700

    3 test completed

--- C#Homework_9/Program.cs
+++ C#Homework_9/Program.cs
@@ -41,4 +41,20 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(NaturalSum(m, n));
 */
 
+/*
 //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return 0;
+}
+
+Console.Write("Input positive value of M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input positive value of N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akkerman(m, n));
+*/
