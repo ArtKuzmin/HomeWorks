83c5c31d0d029110c0b22e5d2aa5521886463c12
ar.kuzmin <ar.kuzmin@yandex.ru>
2022-09-17 22:37:31 +0700

    1 test completed

--- C#Homework_9/Program.cs
+++ C#Homework_9/Program.cs
@@ -1,6 +1,21 @@
-﻿
+﻿/*
 // Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 
+void NaturalSequence(int m, int n)
+{
+    if (m <= n)
+    {
+        NaturalSequence(m, n - 1);
+        Console.Write(n + " ");
+    }
+}
+
+Console.Write("Input value of M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input value of N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturalSequence(m,n);
+*/
 
 //Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
